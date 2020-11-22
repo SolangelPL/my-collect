3e139a4cb6522ab4ede72f49b9c0b18cf2b22777
Solangel <sp.lagares31@gmail.con>
2020-11-22 16:01:05 -0500

    commit tree 2ef43630911021583459b1c995103a8952385955

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(students)
+    pupil = [ ]
+    counter = 0 
+        while counter < students.length
+        pupil << yield(students[counter])
+        counter += 1 
+        end 
+    pupil 
+end  
 
